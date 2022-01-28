ed81499ca5158968526f3577b9d9fee82bb14bb6
joav <jim.acero.valcarcel@gmail.com>
2022-01-28 09:31:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My deck
+
+Proyecto para crear mi propio deck(Stream Deck) para productividad.
---
+++ index.js
@@ -0,0 +1,17 @@
+const robot = require("robotjs");
+
+const TIMES = 3;
+let steps = 0;
+
+const interval = setInterval(() => {
+    if (steps < TIMES) {
+        robot.keyTap('f5');
+        console.log('tapped');
+        steps++;
+    }
+    if (steps === TIMES) {
+        clearInterval(interval);
+        console.log('finished');
+        process.exit();
+    }
+}, 3000);
