c288c229b6797ff0c1323c008a25687dd259f177
joav <jim.acero.valcarcel@gmail.com>
2022-01-28 10:58:44 -0500

    MAC test

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // const robot = require("robotjs");
+var os = require('os');
 var ks = require('node-key-sender');
 
 const TIMES = 3;
 let steps = 0;
+
+function hitHotkey(key, modifier) {
+    if (os.type() === 'Darwin') {
+        if (modifier) {
+            return exec(`Script="tell app \\"System Events\\" to keystroke ${key} using ${modifier} down"
+        osascript -e "$Script"`)
+        } else {
+            return exec(`Script="tell app \\"System Events\\" to keystroke ${key}"
+        osascript -e "$Script"`)
+        }
+    } else {
+        if (modifier) {
+            return ks.sendCombination([modifier, key])
+        } else {
+            return ks.sendKey(key)
+        }
+    }
+}
 
 const interval = setInterval(() => {
     if (steps < TIMES) {
