27fe8bd5d82f45c748c72aef262c1abff3afeb61
joav <jim.acero.valcarcel@gmail.com>
2022-02-18 16:53:36 -0500

    Play audio command logic

--- src/commands/media/commands/play-audio.command.ts
+++ src/commands/media/commands/play-audio.command.ts
@@ -1,8 +1,11 @@
+import os from "os";
 import { BaseCommand, ExecuteParams } from "@commands/shared/base.command";
 import { FileRepository } from "@interfaces/file.repository";
 import { Command } from "@models/command";
 import { ParamsData } from "@models/params-data";
 import { getService } from "@services/services";
+import { exec } from "child_process";
+import path from "path";
 
 type PlayAudioParams = ExecuteParams & {
     audio: string;
@@ -27,7 +30,23 @@ export class PlayAudioCommand extends BaseCommand {
     };
 
     async execute(params: PlayAudioParams): Promise<any> {
-        console.log(params);
+        const sound = path.resolve(__dirname, `../../../../files/${params.audio}`);
+        let script = "";
+        const options:any = {};
+        if (os.type() === 'Darwin') {
+            script = "afplay";
+        } else {
+            script = path.resolve(__dirname, `../../../../bin/mplayer/mplayer`);
+            options.shell = "powershell";
+        }
+        exec(`${script} ${sound}`, options, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        })
     }
 
     async getData(): Promise<ParamsData> {
